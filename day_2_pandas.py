05e80c7587afe303abc9c7313ec34b30359642b9
qivkains <qivkains@naver.com>
2020-05-27 11:01:54 +0900

    2일차 업데이트. 주석

--- day_2_pandas.py
+++ day_2_pandas.py
@@ -7,8 +7,10 @@ print(temp['M'])
 temp['T'] = 0
 print(temp)
 
-# temp['T'][temp['M'] > 3] = 1 주석처리
+# temp['T'][temp['M'] > 3] = 1
 
-import matplotlib.pyplot as plt
-plt.plot(temp['A'], dashes=[6,2])
-plt.show()
+import matplotlib.pyplot as plt # 매트플롯라이브러리 plt로 명명
+plt.plot(temp['A'], dashes=[6,2]) # 매트플롯라이브러리 호출, temp의 A 줄 읽기, 점선 색 6칸 공백 2칸 반복
+plt.xlim((0, 5)) # x축 범위 설정
+plt.ylim((0, 10)) # y축 범위 설정
+plt.show() # 그래프 표시
